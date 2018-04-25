9c4daa2130f8859642a5861a18b5cb43d70ff07f
apple <apple@appledeMacBook-Air-2.local>
2018-04-25 15:43:20 +0800

    更新

--- Capp/Capp/BaseClass/WebViewController/BaseWebViewController.m
+++ Capp/Capp/BaseClass/WebViewController/BaseWebViewController.m
@@ -62,31 +62,43 @@
     _progressLayer.frame = CGRectMake(0, 42, SCREEN_WIDTH, 2);
     [self.navigationController.navigationBar.layer addSublayer:_progressLayer];
     
+    //创建并配置WKWebView的相关参数
+    //1.WKWebViewConfiguration:是WKWebView初始化时的配置类，里面存放着初始化WK的一系列属性；
+    //2.WKUserContentController:为JS提供了一个发送消息的通道并且可以向页面注入JS的类，WKUserContentController对象可以添加多个scriptMessageHandler；
+    //3.addScriptMessageHandler:name:有两个参数，第一个参数是userContentController的代理对象，第二个参数是JS里发送postMessage的对象。添加一个脚本消息的处理器,同时需要在JS中添加，window.webkit.messageHandlers.<name>.postMessage(<messageBody>)才能起作用。
     
+    //加载html
+    //loadFileURL方法通常用于加载服务器的HTML页面或者JS，而loadHTMLString通常用于加载本地HTML或者JS
     //webView部分
     WKWebViewConfiguration *configuration = [[WKWebViewConfiguration alloc] init];
     WKUserContentController *userContentController = [[WKUserContentController alloc] init];
     //这里add 添加的js标识  用于处理接受的js通知做判断
     [userContentController addScriptMessageHandler:self name:@"Camera"];
     [userContentController addScriptMessageHandler:self name:@"Share"];
+    
     configuration.userContentController = userContentController;
     
     WKPreferences *preferences = [WKPreferences new];
     preferences.javaScriptCanOpenWindowsAutomatically = YES;
     preferences.minimumFontSize = 40.0;
     configuration.preferences = preferences;
-    
+    //这样初始化设置的字体大大小和注册的调用方法的标识才会生效
     _webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height-64) configuration:configuration];
+    //这样初始化设置的字体大小和注册的调用方法的标识都会无效
+    //    _webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, 10, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height-64-10)];
     
     //加载URL  显示进度条
-        NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.baidu.com"]];
-        [_webView loadRequest:request];
+    //        NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.baidu.com"]];
+    //        [_webView loadRequest:request];
     
     //js交互
-//    NSString *htmlPath = [[NSBundle mainBundle] pathForResource:@"WKWebViewMessageHandler" ofType:@"html"];
-//    NSString *fileURL = [NSString stringWithContentsOfFile:htmlPath encoding:NSUTF8StringEncoding error:nil];
-//    NSURL *baseURL = [NSURL fileURLWithPath:htmlPath];
-//    [_webView loadHTMLString:fileURL baseURL:baseURL];
+    NSString *htmlPath = [[NSBundle mainBundle] pathForResource:@"WKWebViewMessageHandler" ofType:@"html"];
+    NSString *fileURL = [NSString stringWithContentsOfFile:htmlPath encoding:NSUTF8StringEncoding error:nil];
+    NSURL *baseURL = [NSURL fileURLWithPath:htmlPath];
+    [_webView loadHTMLString:fileURL baseURL:baseURL];
+    
+    
+    
     
     _webView.backgroundColor = [UIColor lightGrayColor];
     _webView.navigationDelegate = self;
@@ -103,11 +115,19 @@
 }
 
 // 页面加载完成之后调用
-- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation
+-(void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation
 {
     [_progressLayer finishedLoad];
     //自动获取  webView的title
     self.navigationItem.title = webView.title;
+    
+    
+    NSString *returnJSStr1 = [NSString stringWithFormat:@"cameraResult('%@')", @"webView加载完成,oc传数据给js"];
+    [_webView evaluateJavaScript:returnJSStr1 completionHandler:^(id _Nullable result, NSError * _Nullable error) {
+        
+        NSLog(@"oc调用js回调,,result==%@,error==%@", result, error);
+    }];
+    
 }
 
 //页面加载失败时 调用
@@ -161,6 +181,13 @@
 }
 
 #pragma mark - WKUIDelegate
+#warning 注意  不实现这个代理  无法实现oc调用js
+/*
+ 不实现这个代理  无法实现oc调用js   调用会没有效果
+ [_webView evaluateJavaScript:returnJSStr completionHandler:^(id _Nullable result, NSError * _Nullable error) {
+ NSLog(@"oc调用js回调,,result==%@,error==%@", result, error);
+ }];
+ */
 - (void)webView:(WKWebView *)webView runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(void))completionHandler
 {
     
@@ -173,6 +200,12 @@
 }
 
 #pragma mark - WKScriptMessageHandler
+/**
+ *  JS 调用 OC 时 webview 会调用此方法
+ *
+ *  @param userContentController  webview中配置的userContentController 信息
+ *  @param message                JS执行传递的消息
+ */
 - (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message
 {
     NSLog(@"message.name:%@", message.name);
@@ -195,6 +228,7 @@
         }];
     }
     if ([message.name isEqualToString:@"Camera"]) {
+#warning 注意格式   例如:cameraResult 形式  XXXResult形式声明方法名
         NSString *returnJSStr = [NSString stringWithFormat:@"cameraResult('%@')", @"调用相册成功,oc传数据给js"];
         [_webView evaluateJavaScript:returnJSStr completionHandler:^(id _Nullable result, NSError * _Nullable error) {
             
